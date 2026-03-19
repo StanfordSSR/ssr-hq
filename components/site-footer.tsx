7c87272e2366b6de529bed5f9d6aec3c139b925a
Artash-N <artash.nath@gmail.com>
2026-03-19 13:16:55 -0700

    Update footer copyright notice

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -2,7 +2,7 @@ export function SiteFooter() {
   return (
     <footer className="site-footer">
       <div className="site-footer-inner">
-        <p>All rights reserved (c) Stanford Robotics Club</p>
+        <p>All rights reserved © Stanford Robotics Club</p>
         <small>This website and associated workflows were put together by Artash Nath (CO &apos;2028)</small>
       </div>
     </footer>
